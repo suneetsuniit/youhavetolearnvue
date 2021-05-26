b2b55cedf6ceeb5e7a652b68d5ac69926fb73a3f
suneetsuniit <84614749+suneetsuniit@users.noreply.github.com>
2021-05-26 17:50:22 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ export default {
   name: "App",
   data() {
     return {
-      value: "Everything's mental. You create your reality with your thoughts. , To be a CHAMPION you have to believe in youself when nobody else will.."
+      value: "Everything's mental. You create your reality with your thoughts. , To be a CHAMPION you have to believe in youself when nobody else will.., If you are afraid of failing, you won't get very far!, To Do Very Hard Things Or For Something That Seems Like Sort Of An Impossible Goal Is To Just Steadily Broaden Your Comfort Zone Over Time To Basically Consistently Do Things That Are Slightly Harder For You, In The Right Direction Not Necessarily Specifically Towards That Goal But Sort Of Keep Broadening Yourself Until Eventually It Seems Possible."
     };
   }
 };
